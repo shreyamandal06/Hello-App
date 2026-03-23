50493c61d90f6abfbaecaf16821e8a7b1b7911cf
Shreya <mandalshreya06@gmail.com>
2026-03-23 22:17:06 +0530

    UC4: print Hello with multiple command-line arguments

---
+++ UC4.java
@@ -0,0 +1,26 @@
+/**
+ * Hello App
+ * UC4
+ * Greet multiple users
+ * 
+ *@author Shreya
+ *@version 4
+ */
+ 
+ public class UC4 {
+    public static void main(String[] args) {
+
+        String message;
+
+        if (args.length > 0) {
+            String names = String.join(", ", args);
+            message = "Hello, " + names + "!";
+        } else {
+            message = "Hello, World!";
+        }
+
+        System.out.println(message);
+    }
+}
+ 
+ 
